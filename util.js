086836afc39a4e3a8dffabc0af283a0fa1bdbbc4
Leontin Kremer <kremer.leontin@gmail.com>
2022-10-27 06:53:26 +0200

    Create util.js

---
+++ util.js
@@ -0,0 +1,13 @@
+let developer = {
+  name: "125",
+  job: "Front-End Developer",
+  experience: "24 month",
+  jobAllInfo: {
+    type: "Front-End",
+    framework: "react",
+  },
+};
+
+const keyName = "name";
+
+console.log(Object.values(developer[keyName]));
